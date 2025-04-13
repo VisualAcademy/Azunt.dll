3ea97acc7bb2dbd0b9b406157be519cad38923fb
Park YongJune <redplus@redplus.net>
2025-04-14 04:29:17 +0900

    Create Azunt class library and MSTest project with IP range utility

---
+++ src/Azunt/Azunt.Networking.Tests/IpAddressUtilityTests.cs
@@ -0,0 +1,39 @@
+﻿using Azunt.Networking;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Azunt.Networking.Tests
+{
+    [TestClass]
+    public class IpAddressUtilityTests
+    {
+        [TestMethod]
+        public void IsIpInRange_ShouldReturnTrue_WhenIpIsInRange()
+        {
+            Assert.IsTrue(IpAddressUtility.IsIpInRange("192.168.0.100", "192.168.0.1", "192.168.0.255"));
+        }
+
+        [TestMethod]
+        public void IsIpInRange_ShouldReturnFalse_WhenIpIsBelowRange()
+        {
+            Assert.IsFalse(IpAddressUtility.IsIpInRange("192.168.0.0", "192.168.0.1", "192.168.0.255"));
+        }
+
+        [TestMethod]
+        public void IsIpInRange_ShouldReturnFalse_WhenIpIsAboveRange()
+        {
+            Assert.IsFalse(IpAddressUtility.IsIpInRange("192.168.1.0", "192.168.0.1", "192.168.0.255"));
+        }
+
+        [TestMethod]
+        public void IsIpInRange_ShouldReturnTrue_WhenIpIsStartOfRange()
+        {
+            Assert.IsTrue(IpAddressUtility.IsIpInRange("192.168.0.1", "192.168.0.1", "192.168.0.255"));
+        }
+
+        [TestMethod]
+        public void IsIpInRange_ShouldReturnTrue_WhenIpIsEndOfRange()
+        {
+            Assert.IsTrue(IpAddressUtility.IsIpInRange("192.168.0.255", "192.168.0.1", "192.168.0.255"));
+        }
+    }
+}

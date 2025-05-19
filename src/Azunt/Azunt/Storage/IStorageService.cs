abd490554305c4770c81c9e4f4c078615c0669e0
Park YongJune <redplus@redplus.net>
2025-05-19 10:26:45 +0900

    Add IStorageService interface for abstracting file storage operations

---
+++ src/Azunt/Azunt/Storage/IStorageService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Azunt.Storage
+{
+    public interface IStorageService
+    {
+        Task<string> UploadAsync(Stream stream, string fileName);
+        Task<Stream> DownloadAsync(string fileName);
+        Task DeleteAsync(string fileName);
+    }
+}

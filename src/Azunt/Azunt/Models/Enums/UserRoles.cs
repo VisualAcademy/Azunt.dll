04a7b55625bf1334f7f64118236349b02770e82f
Park YongJune <redplus@redplus.net>
2025-05-21 03:26:26 +0900

    Add Roles enum to Azunt.Models.Enums for standard user role definitions

---
+++ src/Azunt/Azunt/Models/Enums/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿namespace Azunt.Models.Enums
+{
+    /// <summary>
+    /// 기본 제공되는 사용자 역할 (ASP.NET Core Identity 기반)
+    /// </summary>
+    public enum Roles
+    {
+        Administrators,
+        Everyone,
+        Users,
+        Guests
+    }
+}

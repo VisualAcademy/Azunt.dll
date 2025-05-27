6b6b91fa35900ae0e3f99ab9a5967c7efd4b70f8
Park YongJune <redplus@redplus.net>
2025-05-27 17:33:59 +0900

    Add RemoveLoginViewModel for managing external login removal

---
+++ src/Azunt/Azunt/Models/ManageViewModels/RemoveLoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Azunt.Models.ManageViewModels
+{
+    /// <summary>
+    /// ViewModel used to represent external login information 
+    /// for removing a linked external login from a user's account.
+    /// </summary>
+    public class RemoveLoginViewModel
+    {
+        /// <summary>
+        /// The name of the external login provider (e.g., Google, Facebook).
+        /// </summary>
+        public string LoginProvider { get; set; }
+
+        /// <summary>
+        /// The unique key provided by the external login provider to identify the user.
+        /// </summary>
+        public string ProviderKey { get; set; }
+    }
+}

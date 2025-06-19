4a60f96c679b44215466fd6ca4586e5761d31fa9
Park YongJune <redplus@redplus.net>
2025-06-20 01:01:13 +0900

    Add UserRoleEditViewModel and UserRoleListViewModel to UserRoleViewModels folder

---
+++ src/Azunt/Azunt/Models/UserRoleViewModels/UserRoleEditViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Azunt.Models.UserRoleViewModels
+{
+    public class UserRoleEditViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}

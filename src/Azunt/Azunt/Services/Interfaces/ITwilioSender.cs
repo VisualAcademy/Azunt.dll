f44a02bf769eceec58c313634d9ad7325bd5201d
Park YongJune <redplus@redplus.net>
2025-07-02 01:50:00 +0900

    .

--- src/Azunt/Azunt/Services/Interfaces/ITwilioSender.cs
+++ src/Azunt/Azunt/Services/Interfaces/ITwilioSender.cs
@@ -2,6 +2,9 @@
 
 namespace Azunt.Services // All.Services
 {
+    /// <summary>
+    /// ITwilioSender 서비스 인터페이스 추가: https://youtu.be/5MrgBXJ1Va8
+    /// </summary>
     public interface ITwilioSender
     {
         Task SendSmsAsync(string phoneNumber, string message);

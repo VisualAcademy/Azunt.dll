1cb39bc6ebaef657ce6ecdb74abcea1463da86be
Park YongJune <redplus@redplus.net>
2025-07-02 01:40:21 +0900

    .

--- src/Azunt/Azunt/Services/Interfaces/ISmsSender.cs
+++ src/Azunt/Azunt/Services/Interfaces/ISmsSender.cs
@@ -2,6 +2,9 @@
 
 namespace Azunt.Services
 {
+    /// <summary>
+    /// ISmsSender 인터페이스 파일 추가: https://youtu.be/nTDjYUqpuhY
+    /// </summary>
     public interface ISmsSender
     {
         Task SendSmsAsync(string number, string message);

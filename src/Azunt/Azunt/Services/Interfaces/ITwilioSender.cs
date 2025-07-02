4b728f7076b98ca3684277bf154b29b40d654e64
Park YongJune <redplus@redplus.net>
2025-07-03 03:02:53 +0900

    .

--- src/Azunt/Azunt/Services/Interfaces/ITwilioSender.cs
+++ src/Azunt/Azunt/Services/Interfaces/ITwilioSender.cs
@@ -4,6 +4,7 @@ namespace Azunt.Services // All.Services
 {
     /// <summary>
     /// ITwilioSender 서비스 인터페이스 추가: https://youtu.be/5MrgBXJ1Va8
+    /// ITwilioSender 인터페이스 파일을 Azunt 프로젝트에 추가: https://youtu.be/JiXhL9eXFyM
     /// </summary>
     public interface ITwilioSender
     {
